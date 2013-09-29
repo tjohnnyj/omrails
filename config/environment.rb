3667ae2b8f037fe8b706e19b23fa0a281500459e
tjohnnyj <trace.johnson@gmail.com>
2013-09-28 22:33:42 -0400

    add   RAILS_ENV=production bundle exec rake assets:precompile

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,6 @@
 require File.expand_path('../application', __FILE__)
 
 RAILS_GEM_VERSION = '3.2.11' unless defined? RAILS_GEM_VERSION
-
+  RAILS_ENV=production bundle exec rake assets:precompile
 # Initialize the Rails application.
 Omrails::Application.initialize!
